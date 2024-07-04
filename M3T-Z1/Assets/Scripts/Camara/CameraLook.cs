a0aa3302a412af5ad246185c612a47755501ce70
waza el programador <santivelasquez1409@gmail.com>
2024-07-04 16:49:42 -0500

    CONFIGURACION CAMARA PRIMERA PERSONA
    SE CONFIGURO LA CAMARA PARA PODER MIRAR EN DIRECCIÓN X, Y  CON EL MOUSE

--- M3T-Z1/Assets/Scripts/Camara/CameraLook.cs
+++ M3T-Z1/Assets/Scripts/Camara/CameraLook.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CAMERALOOK : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    public float mouseSensitivity= 80f ; 
+    public Transform playerBody; 
+     
+    float xRotation =0; 
+
     void Start()
     {
-        
+        Cursor.lockState= CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
+
+        xRotation += mouseY;
+
+        xRotation =Mathf.Clamp(xRotation,-90f, 90f);
+
+        transform.localRotation= Quaternion.Euler(xRotation,0,0);
+        playerBody.Rotate(Vector3.up*mouseX);
     }
 }
