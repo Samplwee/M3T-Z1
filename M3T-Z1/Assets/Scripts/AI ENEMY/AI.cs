2857a40c210b36b8491d3ee6e656d8f3f64f49f0
waza el programador <santivelasquez1409@gmail.com>
2024-08-08 20:28:53 -0500

    Malla de enemigo e inteligencia artificial del enemigo
    se puso la ia del enemigo y la malla para que no se pueda tirar al avismo el enemigo se configuro la velocidad en 5 se puede cambiar todas las variables quedaron publicas

---
+++ M3T-Z1/Assets/Scripts/AI ENEMY/AI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+ 
+    public NavMeshAgent navMeshAgent;
+    
+    public GameObject destination1;
+    
+    public GameObject destination2;
+    
+
+    void Start()
+    {
+        navMeshAgent.destination =destination1.transform.position;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float distance =Vector3.Distance(transform.position, destination1.transform.position);
+        if (distance<2)
+        {
+            navMeshAgent.destination =destination1.transform.position;
+        }        
+    }
+
+
+}
