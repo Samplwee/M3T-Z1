5a33d1b336e4095b109836653acc404fda75171e
waza el programador <santivelasquez1409@gmail.com>
2024-09-04 08:41:09 -0500

    cambios ya
    se agrega el daño

--- M3T-Z1/Assets/Scripts/Player/PlayerInteractions.cs
+++ M3T-Z1/Assets/Scripts/Player/PlayerInteractions.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour
 {
+
+    public Transform starposition;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("GunAmmo"))
         {
-            GameManager.Instance.gunAmmo += other.gameObject.GetComponent<AmmoBox>().ammo; 
+            GameManager.Instance.gunAmmo += other.gameObject.GetComponent<AmmoBox>().ammo;
             //Al jugador se le van a heredad los componentes del archivo GameManager este dice que las cajas de municion contienen 10 Balas entonces esa sera la cantidad añadida
 
             Destroy(other.gameObject);
         }
-    }
+
+        if (other.gameObject.CompareTag("Deathfloor"))
+
+            //Daño, Reset 
+            Debug.Log("Deathfloor");
+
+        GameManager.Instance.LoseHealth(100);
+        GetComponent<CharacterController>().enabled=false;
+        gameObject.transform.position = starposition.position;
+        GetComponent<CharacterController>().enabled = true;
+    } 
+
+
+
 }
